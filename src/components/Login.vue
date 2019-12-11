b047d83ee750305c605e24d62800c6bfd5226280
tan-alu <786894966@qq.com>
2019-12-11 22:26:56 +0800

    登陆头像的绘制

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="login_container">
         <div class="login_box">
+            <!-- 头像区域 -->
+            <div class="avatar_box">
+                <img src="../assets/logo.jpg" alt="">
+            </div>
+            <!-- 表单区域 -->
+            <el-form>
+                <el-form-item label="密码" prop="pass">
+                    <!-- <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input> -->
+                </el-form-item>
+            </el-form>
 
         </div>
     </div>
@@ -25,6 +35,25 @@ export default {
     position: relative;
     top:50%;
     left: 50%;
-    transform: translate(-50%,-50%)
+    transform: translate(-50%,-50%);
+
+    .avatar_box{
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    padding:10px;
+    background-color:#fff ;
+    position:absolute;
+    left: 50%;
+    transform:translate(-50%,-50%);
+    box-shadow: 0 0 10px #ddd;
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
 }
+}
+
 </style>
