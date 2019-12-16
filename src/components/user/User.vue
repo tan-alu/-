abef71a2f8d03075a2ddfa24329ce72692a2c115
tan-alu <786894966@qq.com>
2019-12-16 17:31:15 +0800

    用户页面绘制

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,6 +1,30 @@
 // User
 <template>
-    <div>用户</div>
+    <div>
+      <!-- 面包屑导航 -->
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      </el-breadcrumb>
+      <!-- card -->
+      <el-card>
+        <el-row :gutter="20">
+          <el-col :span="10">
+            <!-- 输入搜索框 -->
+            <el-input
+              placeholder="请输入内容"
+              class="input-with-select">
+              <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+          </el-col>
+          <el-col :span="4">
+            <el-button>添加用户</el-button>
+          </el-col>
+        </el-row>
+      </el-card>
+      <!-- 分页 -->
+    </div>
 </template>
 
 <script>
