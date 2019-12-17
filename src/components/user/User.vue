b0aa300dfb4cf12090d3d61f8aa95fd9fc60c59d
tan-alu <786894966@qq.com>
2019-12-17 17:48:13 +0800

    邮箱 手机验证规则

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -113,6 +113,25 @@
 <script>
 export default {
   data () {
+    // 验证邮箱的规则
+    var checkEmail = (rule, value, cb) => {
+      // 验证邮箱的正则表达式
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        // 合法的邮箱
+        return cb()
+      }
+      cb(new Error('请输入合法的邮箱'))
+    },
+    //验证手机号的规则
+    var checkMobile = (rule,value,cb)=>{
+      //验证手机号的正则表达式
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if(regMobile.test(value)){
+        return cb();
+      }
+      cb(new Error('请输入合法的手机号'))
+    }
     return {
       queryInfo: {
         'query': '',
