914d830b4fbbb94ee7bfc3249797dc80d78ad2c9
tan-alu <786894966@qq.com>
2019-12-23 21:09:49 +0800

    表格优化

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -8,11 +8,20 @@
     </el-breadcrumb>
     <!-- card视图 -->
     <el-card>
-        <el-table :data="rightsList">
+        <el-table :data="rightsList"
+            border
+            stripe
+            >
             <el-table-column type="index"></el-table-column>
             <el-table-column label="权限名称" prop="authName"></el-table-column>
             <el-table-column label="路径" prop="path"></el-table-column>
-            <el-table-column label="权限等级" prop="level"></el-table-column>
+            <el-table-column label="权限等级" prop="level">
+                <template slot-scope="scope">
+                    <el-tag v-if="scope.row.level==='0'">一级</el-tag>
+                    <el-tag type="success" v-else-if="scope.row.level==='1'">二级</el-tag>
+                    <el-tag type="warning" v-else>三级</el-tag>
+                </template>
+            </el-table-column>
 
         </el-table>
     </el-card>
