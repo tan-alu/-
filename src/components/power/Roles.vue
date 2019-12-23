79e2b1b1bdfd4c490be248268ef9e9182c2c5404
tan-alu <786894966@qq.com>
2019-12-23 21:30:33 +0800

    角色列表响应数据以及渲染页面

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -1,9 +1,50 @@
 <template>
-    <div>角色</div>
+    <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+        <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- card卡片区域 -->
+    <el-card>
+        <el-table
+            :data="roleList"
+            border
+            stripe
+            >
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="角色名称" prop="roleName"></el-table-column>
+            <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+            <el-table-column label="操作"></el-table-column>
+        </el-table>
+    </el-card>
+    </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 角色列表
+      roleList: []
+    }
+  },
+  mounted () {
+    this.getList()
+  },
+  methods: {
+    //   获取角色列表数据
+    async getList () {
+      const { data: res } = await this.$http.get('roles')
+      //   console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.$message.success('获取角色列表成功')
+      this.roleList = res.data
+    }
+  }
 
 }
 </script>
