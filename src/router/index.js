2e795fcff822186013f8cf1db634a0394d36a718
tan-alu <786894966@qq.com>
2019-12-23 21:18:32 +0800

    通过路由实现角色列表

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import User from '../components/user/User.vue'
 import Right from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 
 Vue.use(VueRouter)
 
@@ -18,7 +19,8 @@ const routes = [
     children: [
       { path: '/welcome', component: Welcome },
       { path: '/users', component: User },
-      { path: '/rights', component: Right }
+      { path: '/rights', component: Right },
+      { path: '/roles', component: Roles }
 
     ] }
 ]
