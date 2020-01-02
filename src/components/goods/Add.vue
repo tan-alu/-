404a27f389a48f19d5c2c6bd11903ca24d8edfc6
tan-alu <786894966@qq.com>
2020-01-02 22:56:43 +0800

    商品分类-绘制基本信息面板的Ui结构

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -28,13 +28,28 @@
 
             </el-steps>
             <!-- tab栏区域 -->
-            <el-tabs :tab-position="'left'" style="height: 200px;" v-model="activeIndex">
-                <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
-                <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
-                <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
-                <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-                <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-            </el-tabs>
+            <el-form v-model="addForm" :rules="addFormRules" ref="ruleForm" label-width="80px" label-position="top">
+                <el-tabs :tab-position="'left'"  v-model="activeIndex">
+                    <el-tab-pane label="基本信息" name="0">
+                        <el-form-item label="商品名称" prop="goods_name">
+                            <el-input v-model="addForm.goods_name"></el-input>
+                        </el-form-item>
+                        <el-form-item label="商品价格" prop="goods_price">
+                            <el-input v-model="addForm.goods_price" type="number"></el-input>
+                        </el-form-item>
+                        <el-form-item label="商品重量" prop="goods_weight">
+                            <el-input v-model="addForm.goods_weight"></el-input>
+                        </el-form-item>
+                        <el-form-item label="商品数量" prop="goods_number">
+                            <el-input v-model="addForm.goods_number"></el-input>
+                        </el-form-item>
+                    </el-tab-pane>
+                    <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+                    <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+                    <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+                    <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+                </el-tabs>
+            </el-form>
 
       </el-card>
     </div>
@@ -44,7 +59,20 @@
 export default {
   data () {
     return {
-      activeIndex: '0'
+      activeIndex: '0',
+      // 添加商品的表单数据对象
+      addForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0
+      },
+      addFormRules: {
+        goods_name: [{ required: true, message: '请输入商品名称', trigger: 'blur' }],
+        goods_price: [{ required: true, message: '请输入商品价格', trigger: 'blur' }],
+        goods_weight: [{ required: true, message: '请输入商品重量', trigger: 'blur' }],
+        goods_number: [{ required: true, message: '请输入商品数量', trigger: 'blur' }]
+      }
     }
   }
 
